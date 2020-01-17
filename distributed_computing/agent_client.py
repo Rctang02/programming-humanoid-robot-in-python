8ac72233924c2f9fdf7aeef7aceb83ccd2199e38
Ruochen Tang <ruochen.tang@campus.tu-berlin.de>
2020-01-17 20:45:11 +0100

    distributed computing

--- distributed_computing/agent_client.py
+++ distributed_computing/agent_client.py
@@ -7,6 +7,8 @@
 '''
 
 import weakref
+import threading
+import xmlrpclib
 
 class PostHandler(object):
     '''the post hander wraps function to be excuted in paralle
@@ -16,11 +18,13 @@ class PostHandler(object):
 
     def execute_keyframes(self, keyframes):
         '''non-blocking call of ClientAgent.execute_keyframes'''
-        # YOUR CODE HERE
+        exe_keyframe_ = threading.Thread(target=self.proxy.execute_keyframes, args=keyframes)
+        exe_keyframe_.start()
 
     def set_transform(self, effector_name, transform):
         '''non-blocking call of ClientAgent.set_transform'''
-        # YOUR CODE HERE
+        set_transform_ = threading.Thread(target=self.proxy.set_transform, args=[effector_name, transform])
+        set_transform_.start()
 
 
 class ClientAgent(object):
@@ -29,35 +33,36 @@ class ClientAgent(object):
     # YOUR CODE HERE
     def __init__(self):
         self.post = PostHandler(self)
-    
+        self.server  = xmlrpclib.ServerProxy("http://localhost:8888")
+
     def get_angle(self, joint_name):
         '''get sensor value of given joint'''
-        # YOUR CODE HERE
+        return self.server.get_angle(joint_name)
     
     def set_angle(self, joint_name, angle):
         '''set target angle of joint for PID controller
         '''
-        # YOUR CODE HERE
+        self.server.set_angle(joint_name, angle)
 
     def get_posture(self):
         '''return current posture of robot'''
-        # YOUR CODE HERE
+        return self.server.get_posture()
 
     def execute_keyframes(self, keyframes):
         '''excute keyframes, note this function is blocking call,
         e.g. return until keyframes are executed
         '''
-        # YOUR CODE HERE
+        self.post.execute_keyframes(keyframes)
 
     def get_transform(self, name):
         '''get transform with given name
         '''
-        # YOUR CODE HERE
+        return self.server.get_transform(name)
 
     def set_transform(self, effector_name, transform):
         '''solve the inverse kinematics and control joints use the results
         '''
-        # YOUR CODE HERE
+        self.post.set_transform(effector_name, transform)
 
 if __name__ == '__main__':
     agent = ClientAgent()
--- distributed_computing/agent_server.py
+++ distributed_computing/agent_server.py
@@ -17,41 +17,55 @@ import sys
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'kinematics'))
 
 from inverse_kinematics import InverseKinematicsAgent
+from recognize_posture import PostureRecognitionAgent
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+import xmlrpclib
 
 
-class ServerAgent(InverseKinematicsAgent):
+class ServerAgent(InverseKinematicsAgent, PostureRecognitionAgent):
     '''ServerAgent provides RPC service
     '''
     # YOUR CODE HERE
-    
+    def __init__(self):
+        super(ServerAgent, self).__init__()
+        server = SimpleXMLRPCServer(("localhost", 8888))
+        server.register_function(ServerAgent.get_angle, "get_angle")
+        server.register_function(ServerAgent.set_angle, "set_angle")
+        server.register_function(ServerAgent.get_posture, "get_posture")
+        server.register_function(ServerAgent.execute_keyframes, "execute_keyframes")
+        server.register_function(ServerAgent.get_transform, "get_transform")
+        server.register_function(ServerAgent.set_transform, "set_transform")
+        server.register_instance(agent)
+        server.serve_forever()
+
     def get_angle(self, joint_name):
         '''get sensor value of given joint'''
-        # YOUR CODE HERE
+        return self.perception.joint[joint_name]
     
     def set_angle(self, joint_name, angle):
         '''set target angle of joint for PID controller
         '''
-        # YOUR CODE HERE
+        self.perception.joint[joint_name] = angle
 
     def get_posture(self):
         '''return current posture of robot'''
-        # YOUR CODE HERE
+        return self.recognize_posture(self.perception)  #recognize posture??
 
     def execute_keyframes(self, keyframes):
         '''excute keyframes, note this function is blocking call,
         e.g. return until keyframes are executed
         '''
-        # YOUR CODE HERE
+        self.keyframes = keyframes
 
     def get_transform(self, name):
         '''get transform with given name
         '''
-        # YOUR CODE HERE
+        return self.transforms[name]
 
     def set_transform(self, effector_name, transform):
         '''solve the inverse kinematics and control joints use the results
         '''
-        # YOUR CODE HERE
+        self.set_transforms(effector_name, transform)
 
 if __name__ == '__main__':
     agent = ServerAgent()
